3a2c0b985af8ed7e3572a5bd997c87c7d33e271b
Takahiro Hirakawa <jonhirak@yahoo.com>
2021-10-29 17:54:00 -0700

    Add nodemon dev dependency resolve static file path

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
 
-app.use(express.static('../dist'))
+const app = express();
+const port = 3000;
+const path = require('path');
 
-app.use(express.json())
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
