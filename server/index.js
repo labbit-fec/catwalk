2a12d98643e42100fde82c951f9ddf4c9de2a2e5
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-11-01 15:46:21 -0400

    create server route to get stars count and render on client side

--- server/index.js
+++ server/index.js
@@ -4,9 +4,13 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
+const axios = require('axios');
+const { TOKEN } = require('./config');
+
 app.use(express.static(path.join(__dirname, '../dist')));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
@@ -17,3 +21,32 @@ app.listen(port, () => {
 // })
 
 module.exports.port = port;
+const server = {
+  baseUrl: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo',
+  authorization: TOKEN,
+};
+
+app.get('/api/reviews/stars', (req, res) => {
+  const { productId } = req.query;
+  axios
+    .get(`${server.baseUrl}/reviews/meta`, {
+      params: {
+        product_id: productId,
+      },
+      headers: {
+        Authorization: server.authorization,
+      },
+    })
+    .then((response) => {
+      const { ratings } = response.data;
+      const count = Object.keys(ratings).reduce(
+        (previous, current) => previous + ratings[current],
+        0
+      );
+      const sum = Object.keys(ratings).reduce(
+        (previous, current) => previous + current * ratings[current],
+        0
+      );
+      res.status(200).json({ stars: 3 });
+    });
+});
--- src/components/ratings/RatingSummary/Stars/Stars.jsx
+++ src/components/ratings/RatingSummary/Stars/Stars.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import styles from './Stars.css';
 import StarGraphic from './StarGraphic/StarGraphic';
 
 export default function Stars() {
+  const [stars, setStars] = useState(0);
+
+  function getStars() {
+    return axios.get('/api/reviews/stars', {
+      params: {
+        productId: 61575,
+      },
+    });
+  }
+
+  useEffect(() => {
+    getStars()
+      .then((response) => {
+        setStars(response.data.stars);
+        console.log(response.data.stars);
+      })
+      .catch((err) => console.log(err));
+  });
+
   return (
     <div className={styles.container}>
-      <div className={styles.starCount}>3.5</div>
-      <StarGraphic stars={3.5} />
+      <div className={styles.starCount}>{stars}</div>
+      <StarGraphic stars={stars} />
     </div>
   );
 }
