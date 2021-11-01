5e7c956c3181cc462c32de809b74e70ec7bfa44a
Takahiro Hirakawa <jonhirak@yahoo.com>
2021-11-01 15:26:12 -0700

    Add Answer component

---
+++ src/components/q&a/QuestionsList/IndividualQuestion/Answer/Answer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Answer = function () {
+  return (
+    <div className="answer">
+      <span className="answer_text">
+        <h6>A: </h6>
+        <span> Some answer to the question. </span>
+      </span>
+    </div>
+  );
+};
+
+export default Answer;
--- src/components/q&a/QuestionsList/IndividualQuestion/IndividualQuestion.jsx
+++ src/components/q&a/QuestionsList/IndividualQuestion/IndividualQuestion.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import Answer from './Answer/Answer';
 
 const IndividualQuestion = function (props) {
   return (
     <div className="individual_question">
-      <h6 className="question">Q: Some question about product?</h6>
+      <div className="question">
+        <h6 className="question_text">Q: Some question about product?</h6>
+        <span className="question_helpful"> Helpful? </span>|
+        <span className="add_answer"> Add Answer</span>
+      </div>
+      <Answer />
+      <hr />
     </div>
   );
 };
