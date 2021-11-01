251f952ecf3ee70fa80b984ab3be9a0c5ba1b19f
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-10-31 21:01:50 -0400

    implement Stars component

--- src/components/ratings/RatingSummary/Stars/Stars.jsx
+++ src/components/ratings/RatingSummary/Stars/Stars.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { VscStarEmpty, VscStarFull, VscStarHalf } from 'react-icons/vsc';
 import styles from './Stars.css';
 
 export default function Stars() {
   return (
     <div className={styles.container}>
-      <h3>Stars</h3>
+      <div className={styles.starCount}>3.5</div>
+      <div className={styles.starIcons}>
+        <VscStarFull />
+        <VscStarFull />
+        <VscStarFull />
+        <VscStarHalf />
+        <VscStarEmpty />
+      </div>
     </div>
   );
 }
