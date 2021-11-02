162c5297f9819cd9847545ed5a871f33f532818d
Kevin Goble <goble.kevin@gmail.com>
2021-11-02 14:14:26 -0700

    Displays ProductInformation, ProductTextOverview, and StyleSelector Containers

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import styles from './Overview.css';
 import ImageGallery from './ImageGallery/ImageGallery';
 import AddToCart from './AddToCart/AddToCart';
+import ProductInformation from './ProductInformation/ProductInformation';
+import StyleSelector from './StyleSelector/StyleSelector';
+import ProductTextOverview from './ProductTextOverview/ProductTextOverview';
 
 const Overview = () => (
   <div className={styles.container}>
     Product Overview
     <ImageGallery />
+    <ProductInformation />
+    <StyleSelector />
     <AddToCart />
+    <ProductTextOverview />
   </div>
 );
 
---
+++ src/components/overview/StyleSelector/StyleSelector.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './StyleSelector.css';
+
+const StyleSelector = () => (
+  <div className={styles.container}>
+    Style Selector
+    <div />
+  </div>
+);
+
+export default StyleSelector;
