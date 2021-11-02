1a0d5746e102f153739d9f739dbf72e7f621c874
Kevin Goble <goble.kevin@gmail.com>
2021-11-02 12:54:16 -0700

    Displays Overview Container

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import styles from './Overview.css';
+import ImageGallery from './ImageGallery/ImageGallery';
+import AddToCart from './AddToCart/AddToCart';
 
-const Overview = () => <div className={styles.container}>Product Overview</div>;
+const Overview = () => (
+  <div className={styles.container}>
+    Product Overview
+    <ImageGallery />
+    <AddToCart />
+  </div>
+);
 
 export default Overview;
