f1c8597733f5ed6027d8b5fe819cd0ca4a4a9f96
Kevin Goble <goble.kevin@gmail.com>
2021-11-02 15:13:12 -0700

    Image Gallery displays Main photo

--- src/components/overview/ImageGallery/ImageGallery.jsx
+++ src/components/overview/ImageGallery/ImageGallery.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import styles from './ImageGallery.css';
 
 const ImageGallery = () => (
-  <div className={styles.container}>Image Gallery</div>
+  <div className={styles.container}>
+    Image Gallery
+    <img
+        className={styles.img}
+        src="https://images.unsplash.com/photo-1613758479550-c3fe095bd333?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+        alt="Corgi Wearing Warm Coat in a Snowy Forest"
+      />
+  </div>
 );
 
 export default ImageGallery;
