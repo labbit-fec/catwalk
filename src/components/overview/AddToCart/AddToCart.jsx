1de5c2b24b1bffb4b918b325f8003eb68f2d2be2
Kevin Goble <goble.kevin@gmail.com>
2021-11-02 12:55:24 -0700

    Displays AddToCart Container

---
+++ src/components/overview/AddToCart/AddToCart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './AddToCart.css';
+
+const AddToCart = () => (
+  <div className={styles.container}>
+    Add To Cart
+    <div />
+  </div>
+);
+
+export default AddToCart;
