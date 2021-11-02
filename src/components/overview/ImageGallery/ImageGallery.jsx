19791f3d416130d63ee5ef13a981b57496778189
Kevin Goble <goble.kevin@gmail.com>
2021-11-02 12:54:53 -0700

    Displays ImageGallery Container

---
+++ src/components/overview/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './ImageGallery.css';
+
+const ImageGallery = () => (
+  <div className={styles.container}>Image Gallery</div>
+);
+
+export default ImageGallery;
