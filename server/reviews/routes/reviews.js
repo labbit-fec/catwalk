324e890d00fce3e915e4b7959e6615109b32d60d
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-11-02 14:29:09 -0400

    add /reviews/firsTwoReviews route that returns data on first two reviews

--- server/reviews/routes/reviews.js
+++ server/reviews/routes/reviews.js
@@ -4,4 +4,22 @@ const { baseUrl, authorization } = require('../../server-config');
 
 const router = express.Router();
 
+router.get('/firstTwoReviews', (req, res) => {
+  const { productId } = req.query;
+  axios
+    .get(`${baseUrl}/reviews`, {
+      params: {
+        product_id: productId,
+        page: 1,
+        count: 2,
+      },
+      headers: {
+        Authorization: authorization,
+      },
+    })
+    .then((response) => {
+      res.status(200).json({ reviews: response.data.results });
+    });
+});
+
 module.exports = router;
