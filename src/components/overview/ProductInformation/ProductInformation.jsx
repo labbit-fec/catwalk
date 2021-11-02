783bd5b2e891c2c08ecd668b957484293942eb85
Kevin Goble <goble.kevin@gmail.com>
2021-11-02 15:43:58 -0700

    Displays internal Product Information

--- src/components/overview/ProductInformation/ProductInformation.jsx
+++ src/components/overview/ProductInformation/ProductInformation.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './ProductInformation.css';
+import StarGraphic from '../../ratings/RatingSummary/Stars/StarGraphic/StarGraphic';
 
 const ProductInformation = () => (
   <div className={styles.container}>
     Product Information
-    <div />
+    <StarGraphic stars={4} />
+    <div className={styles.category}>Category</div>
+    <h1 className={styles.title}>Product Title</h1>
+    <div className={styles.price}>$395</div>
   </div>
 );
 
