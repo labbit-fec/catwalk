bd7fd8b7a86489720993efdc0050a06ec02a0df0
Kevin Goble <goble.kevin@gmail.com>
2021-11-02 16:16:39 -0700

    Displays internal components of Style Selector

--- src/components/overview/StyleSelector/StyleSelector.jsx
+++ src/components/overview/StyleSelector/StyleSelector.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import styles from './StyleSelector.css';
+import StylePicker from './StylePicker/StylePicker';
 
 const StyleSelector = () => (
   <div className={styles.container}>
     Style Selector
-    <div />
+    <div className={styles.title}>
+      {'Style >'}
+      <div>SELECTED STYLE</div>
+    </div>
+    <StylePicker />
   </div>
 );
 
