9dd85a6b40615ffffa1ebc0babbf3cfe993894d8
Takahiro Hirakawa <jonhirak@yahoo.com>
2021-11-03 10:24:59 -0700

    Add beforeEach in QA test to render the component just once

--- src/components/q&a/q&a.test.jsx
+++ src/components/q&a/q&a.test.jsx
@@ -10,23 +10,21 @@ import {
 import '@testing-library/jest-dom';
 import QA from './QA';
 
+beforeEach(() => {
+  render(<QA />);
+});
+
 // eslint-disable-next-line no-undef
 test('Displays QA Container', async () => {
-  render(<QA />);
-
   expect(screen.getByText('Questions & Answers')).toBeVisible();
 });
 
 test('Displays Search element', async () => {
-  render(<QA />);
-
   expect(
     screen.getByPlaceholderText('Search Questions & Answers')
   ).toBeVisible();
 });
 
 test('Displays questions list container', async () => {
-  render(<QA />);
-
   expect(screen.getByTestId('question-list-container')).toBeVisible();
 });
