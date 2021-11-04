efb03d6a1c615c4f59be731238a1880788364bff
Takahiro Hirakawa <jonhirak@yahoo.com>
2021-11-04 12:41:53 -0700

    Dynamically render images

--- src/components/q&a/QuestionsList/IndividualQuestion/Answer/Images/Images.jsx
+++ src/components/q&a/QuestionsList/IndividualQuestion/Answer/Images/Images.jsx
@@ -6,23 +6,8 @@ const Images = function ({ photos }) {
   return (
     <div className={styles.container}>
       {photos.map((photo) => (
-        <img className={styles.img} src={photo} alt="" />
+        <img className={styles.img} src={photo.url} alt="" />
       ))}
-      {/* <img
-        className={styles.img}
-        src="https://images.unsplash.com/photo-1530519729491-aea5b51d1ee1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80"
-        alt="some dummy"
-      />
-      <img
-        className={styles.img}
-        src="https://images.unsplash.com/photo-1511127088257-53ccfcc769fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-        alt="some dummy"
-      />
-      <img
-        className={styles.img}
-        src="https://images.unsplash.com/photo-1500603720222-eb7a1f997356?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1653&q=80"
-        alt="some dummy"
-      /> */}
     </div>
   );
 };
@@ -30,5 +15,10 @@ const Images = function ({ photos }) {
 export default Images;
 
 Images.propTypes = {
-  photos: PropTypes.arrayOf(PropTypes.string).isRequired,
+  photos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
