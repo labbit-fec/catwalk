2ade59fa4542d9fb8ee53ffd310e98be12dd8439
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-11-07 17:07:08 -0500

    add firebase config

---
+++ server/firebase/index.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCCdb1cpDWjm1LvB9KqSTwvuAR-oCZoo3M',
+  authDomain: 'limitless-25d7a.firebaseapp.com',
+  databaseURL: 'gs://limitless-25d7a.appspot.com',
+  projectId: 'limitless-25d7a',
+  storageBucket: 'limitless-25d7a.appspot.com',
+  messagingSenderId: '10526228847',
+  appId: '1:10526228847:web:8e69b9ea9ca6916d17689a',
+};
