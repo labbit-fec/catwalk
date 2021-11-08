f0fe5f8d514944ed5cf1ebb4938c5d1561f5b7ed
Kevin Goble <goble.kevin@gmail.com>
2021-11-07 21:29:11 -0800

    Refactors ProductTextOverview to display data from props

--- src/components/overview/ProductTextOverview/ProductTextOverview.jsx
+++ src/components/overview/ProductTextOverview/ProductTextOverview.jsx
@@ -1,11 +1,54 @@
+/* eslint-disable react/no-array-index-key */
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './ProductTextOverview.css';
 
-const ProductTextOverview = () => (
-  <div className={styles.container}>
-    Product Text Overview
-    <div />
-  </div>
-);
+const ProductTextOverview = ({ product }) => {
+  const features = [];
+  if (product.features !== undefined) {
+    product.features.forEach((object, key) => {
+      features.push(
+        <div>
+          <span key={key}>{object.feature}</span>
+          <span key={key}>{object.value}</span>
+        </div>
+      );
+    });
+  }
+
+  return (
+    <div className={styles.container}>
+      <div>{product.slogan}</div>
+      <div>{product.description}</div>
+      <div>{features}</div>
+    </div>
+  );
+};
+
+ProductTextOverview.propTypes = {
+  product: PropTypes.shape({
+    slogan: PropTypes.string,
+    description: PropTypes.string,
+    features: PropTypes.arrayOf(
+      PropTypes.shape({
+        feature: PropTypes.string,
+        value: PropTypes.string,
+      })
+    ),
+  }),
+};
+
+ProductTextOverview.defaultProps = {
+  product: {
+    slogan: 'Placeholder Slogan',
+    description: 'This should be a long description',
+    features: [
+      {
+        feature: 'Placeholder feature name',
+        value: 'Placeholder feature value',
+      },
+    ],
+  },
+};
 
 export default ProductTextOverview;
