9f9f2a748516d0e08dcd4096d666eb79da415656
Kevin Goble <goble.kevin@gmail.com>
2021-11-07 16:45:13 -0800

    Sets up function for ProductDetail HOC

---
+++ src/components/overview/ProductDetailWrapper.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const withProductDetails =
+  (WrappedComponent, productId) =>
+  (...props) => {
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+      axios
+        .get(`/products?productId=${productId}`)
+        .then((response) => {
+          setProduct(response.data.Product);
+        })
+        .catch((error) => {
+          console.log('Error getting product details', error);
+        });
+    }, []);
+    // allowed for HOC
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    return <WrappedComponent product={product} {...props} />;
+  };
