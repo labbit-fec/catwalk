2833abd1cf2fe76781b5a9fea61d263e5734ce27
Kevin Goble <goble.kevin@gmail.com>
2021-11-09 20:41:17 -0800

    Refactor Stylepicker to display thumbnails

--- src/components/overview/StyleSelector/StylePicker/StylePicker.jsx
+++ src/components/overview/StyleSelector/StylePicker/StylePicker.jsx
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import PropTypes from 'prop-types';
 import styles from './StylePicker.css';
+import selectedStyleContext from '../../context/SelectedStyleContext';
 
-const StylePicker = () => {
-  const circles = [];
-  for (let index = 0; index < 4; index += 1) {
-    circles.push(<span className={styles.dot}></span>);
+const StylePicker = ({ styleData }) => {
+  const thumbnails = [];
+  const { selectedStyleIndex, setSelectedStyleIndex } =
+    useContext(selectedStyleContext);
+
+  if (styleData[0].name !== undefined) {
+    styleData.forEach((currentStyle) => {
+      thumbnails.push(
+        <img
+          alt={`${currentStyle.name} style thumbnail`}
+          src={currentStyle.photos[0].thumbnail_url}
+        />
+      );
+    });
   }
+
   return (
     <div className={styles.container}>
-      <div className={styles.row}>{circles}</div>
-      <div className={styles.row}>{circles}</div>
+      <div className={styles.row}>{thumbnails}</div>
     </div>
   );
+};
+
+StylePicker.propTypes = {
+  styleData: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      photos: PropTypes.arrayOf(
+        PropTypes.shape({
+          thumbnail_url: PropTypes.string,
+          url: PropTypes.string,
+        })
+      ),
+    })
+  ),
+};
+
+StylePicker.defaultProps = {
+  styleData: [
+    {
+      name: 'Black',
+      photos: [
+        {
+          thumbnail_url: `https://images.unsplash.com/photo-1552902865-b72c031ac5ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80`,
+          url: `https://images.unsplash.com/photo-1552902865-b72c031ac5ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80`,
+        },
+      ],
+    },
+  ],
 };
 
 export default StylePicker;
